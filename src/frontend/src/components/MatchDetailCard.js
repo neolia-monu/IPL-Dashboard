64e019b8c8b521c310714837dcca0b58dbec0460
Monu Neolia <neoliamonu@gmail.com>
2022-05-03 20:22:14 +0530

    added components (frontend)

---
+++ src/frontend/src/components/MatchDetailCard.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+export const MatchDetailCard = () => {
+    return (
+      <div className="MatchDetailedCard">
+          <h3>Latest Matches</h3>
+          <h4>Match Detail</h4>
+      </div>
+    );
+}
