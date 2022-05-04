43aa16bae0cb9a1a8b0e30d4ae50115d893a2892
Monu Neolia <neoliamonu@gmail.com>
2022-05-05 01:07:18 +0530

    added match page frontend

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,10 +1,35 @@
-import { React } from 'react';
+import { React , useState, useEffect } from 'react';
+import { useParams} from "react-router-dom";
+import {MatchDetailCard} from "../components/MatchDetailCard";
 
 export const MatchPage = () => {
+
+    const [matches, setMatches] = useState([]);
+    // const teamName ="Royal Challengers Bangalore";
+    const { teamName, year } = useParams();
+
+    useEffect(
+        () => {
+        const fetchMatches = async () => {
+            const res = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await res.json();
+
+            // console.log(data);
+
+            setMatches(data);
+        };
+
+        fetchMatches();
+    },
+        [teamName, year]
+    );
 
     return (
         <div className="MatchPage">
             <h3>Match Page</h3>
+
+            { matches.map(match => <MatchDetailCard teamName={teamName} match={match} /> ) }
+
         </div>
     );
 }
